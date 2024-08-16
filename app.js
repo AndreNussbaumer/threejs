ad8028946cbd1d985d7f7e5ec5850ee779a8de66
André <andrenussb@gmail.com>
2024-08-16 18:53:28 +0100

    4th commit

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const port = 3000
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
-  res.send('index.html')
-})
-
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname })
+});
 
 // Start the server
 app.listen(port, () => {
